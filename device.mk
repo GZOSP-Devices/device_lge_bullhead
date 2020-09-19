de691cf1df00f978b05a98901d178fffe1155d81
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-09-19 09:59:09 +0000

    bullhead: build health hal

--- device.mk
+++ device.mk
@@ -278,10 +278,9 @@ PRODUCT_PACKAGES += \
     libOmxVdecHevc \
     libOmxVenc
     
-# Health
+# Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-service \
-    android.hardware.health@1.0-impl
+    android.hardware.health@2.0-service.bullhead
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
